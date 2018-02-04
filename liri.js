6b4da831f47507200407a659702e33ad4bbe29a8
Susan Marek <smarek@chaskamn.com>
2018-02-03 19:06:18 -0600

    good functionality for all choices

--- liri.js
+++ liri.js
@@ -58,11 +58,11 @@ inquirer.prompt([
     	},
     	])
     	.then(function(inquirerResponse) {
-    		console.log("NAME " + inquirerResponse.name);
+    		// console.log("NAME " + inquirerResponse.name);
     		mediaString = inquirerResponse.name;
-    		console.log("mediaString" + mediaString);
+    		// console.log("mediaString" + mediaString);
     		if (mediaString === ""){
-    			mediaString = "The Sign";
+    			mediaString = "The Sign by Ace of Base";
     		}
 			displaySpotify();
 		});
@@ -87,62 +87,49 @@ inquirer.prompt([
 		});
      }
      else if(inquirerResponse.command === "do-what-it-says"){
-     	console.log("ANYTHING " + inquirerResponse.command);
+     	// console.log("ANYTHING " + inquirerResponse.command);
+     	displaySelection();
      }
 
 
  });
-
-
-
-
- //  	function mediaName() {
-	// 	// var inquirer = require("inquirer");
-
-	// 	inquirer.prompt([
-	// 	{
-	// 		type: "input",
-	// 		message: "Movie or Song Title?",
-	// 		name: "name"
-	// 	},
-	// 	])
-	// 	.then(function(inquirerResponse) {
-	// 		console.log("NAME " + inquirerResponse.name);
-	// 		mediaString = inquirerResponse.name;
-	// 		console.log("mediaString" + mediaString);
-
-	// 	});
-	// }
+//-------------------------------------------------------------------------------------
 
   // Functions for each liri selection
 
   	function displayTwitter() {
 
-  	var params = {screen_name: mediaString};
-  	client.get('statuses/user_timeline/', params, function(error, data, response) {
-  		if (!error) {
-  			console.log("\nTweets for screen name: " + mediaString);
-  			for (var i = 0;  i < data.length; i++) {
-  				console.log("\n--------------------- tweet " + (i+1) + "------------------------")
-  				console.log(data[i].text);
-  				console.log("Created: " + data[i].created_at + " by " + data[i].user.name);
-  			};  
-  		}
-  		else{
-  			console.log("Error: " + error);
-  			return;
-  		}
-  	});       
+	  	var params = {screen_name: mediaString};
+	  	client.get('statuses/user_timeline/', params, function(error, data, response) {
+	  		if (!error) {
+	  			console.log("\nTweets for screen name: " + mediaString);
+	  			for (var i = 0;  i < data.length; i++) {
+	  				console.log("\n--------------------- tweet " + (i+1) + "------------------------")
+	  				console.log(data[i].text);
+	  				console.log("Created: " + data[i].created_at + " by " + data[i].user.name);
+	  			};  
+	  		}
+	  		else{
+	  			console.log("Error: " + error);
+	  			return;
+	  		}
+	  	});       
 	};
 
 	function displaySpotify() {
-		console.log("mediaString in displaySpotify function: " + mediaString);
+		// console.log("mediaString in displaySpotify function: " + mediaString);
 
-		spotify.search({ type: 'track', query: mediaString, limit: 5 }, function(err, data) {
+		spotify.search({ type: 'track', query: mediaString, limit: 1 }, function(err, data) {
 			if (err) {
 				return console.log('Error: ' + err);
 			} 
-			console.log(data); 
+			else {
+			console.log("\nArtist/s Name: " + data.tracks.items[0].artists[0].name); 
+			console.log("Song Name: " + data.tracks.items[0].name);
+			console.log("Preview Link: " + data.tracks.items[0].preview_url);
+			console.log("Album Name: " + data.tracks.items[0].album.name);
+			// console.log("log: \n" + data.tracks.items[0]);
+			}
 		});
 
 	};
@@ -176,10 +163,47 @@ inquirer.prompt([
 			else{
 	  			console.log("Error: " + error);
 	  			return;
-  		}
+  			}
 		});
 	};
 
 	function displaySelection() {
-           
-    };
+
+
+		var fs = require("fs");
+
+// This block of code will read from the "movies.txt" file.
+// It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+// The code will store the contents of the reading inside the variable "data"
+		fs.readFile("random.txt", "utf8", function(error, data) {
+
+  // If the code experiences any errors it will log the error to the console.
+ 	 	if (error) {
+  			return console.log("Error: " + error);
+  		}
+
+  // We will then print the contents of data
+  		console.log(data);
+
+  // Then split it by commas (to make it more readable)
+  		var dataArr = data.split(",");
+
+  // We will then re-display the content as an array for later use.
+ 		 // console.log(dataArr);
+ 		 if (dataArr[0]==="spotify-this-song"){
+ 		 	mediaString = dataArr[1];
+ 		 	displaySpotify();
+ 		 }
+ 		 else if(dataArr[0]==="movie-this"){
+ 		 	mediaString = dataArr[1];
+ 		 	displayMovie();
+ 		 }
+ 		 else if(dataArr[0]==="my-tweets"){
+ 		 	mediaString = dataArr[1];
+ 		 	displayTwitter();
+ 		 }
+
+ 		 });
+
+
+		}
